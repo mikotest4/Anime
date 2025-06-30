318e058db7d221045d667c3f71ad833f469741ca
Yae Miko <aloksinghkumar756@gmail.com>
2025-06-30 20:14:02 +0530

    Update ffencoder.py

--- bot/core/ffencoder.py
+++ bot/core/ffencoder.py
@@ -13,10 +13,10 @@ from .func_utils import mediainfo, convertBytes, convertTime, sendMessage, editM
 from .reporter import rep
 
 ffargs = {
+    'Hdrip': Var.FFCODE_Hdrip,
     '1080': Var.FFCODE_1080,
     '720': Var.FFCODE_720,
     '480': Var.FFCODE_480,
-    '360': Var.FFCODE_360,
 }
 
 class FFEncoder:
